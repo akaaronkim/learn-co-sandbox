2fad111188120d2dcd71ced4877c879efc56cede
Aaron Kim <akaaronkim@gmail.com>
2019-12-10 17:59:28 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,8 @@
+  archer = {
+      "name" => "Sterling Mallory Archer",
+      "co-workers"=> ["Lana Kane", "Cyril Figgis", "Cheryl Tunt", "Pam Poovey", "Dr Krieger"],
+      "favorite_drink" => "Bloody Mary",
+      "Quotes" => ["I swear to god, I had something for this", "Phrasing", "Boop", "Danger Zone", "Read a book", "Do you not?", "Can't or won't?"]
+  }
+  
+  puts archer["Quotes"].sample
